3ccbee3b29d266ed261d0337d09a40d08f6912d7
Elmer Zapata <elmer.765@gmail.com>
2022-12-06 17:55:32 -0400

    first commit

---
+++ src/test/java/com/cucumber/MyStepdefs.java
@@ -0,0 +1,22 @@
+package com.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("today is Sunday")
+    public void todayIsSunday() {
+        
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void iAskWhetherItSFridayYet() {
+        
+    }
+
+    @Then("I should be told {string}")
+    public void iShouldBeTold(String arg0) {
+
+    }
+}
---
+++ src/test/java/com/cucumber/SeleniumSteps.java
@@ -0,0 +1,48 @@
+package com.cucumber;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SeleniumSteps {
+    private final WebDriver driver = new FirefoxDriver();
+
+    @Given("I am on the Google search page")
+    public void I_visit_google() {
+        driver.get("https://www.google.com");
+    }
+
+    @When("I search for {string}")
+    public void search_for(String query) {
+        WebElement element = driver.findElement(By.name("q"));
+        // Enter something to search for
+        element.sendKeys(query);
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+    }
+
+    @Then("the page title should start with {string}")
+    public void checkTitle(String titleStartsWith) {
+        // Google's search is rendered dynamically with JavaScript
+        // Wait for the page to load timeout after ten seconds
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith(titleStartsWith);
+            }
+        });
+    }
+
+    @After()
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
